Maybe calculating prior covariance wrong?

data_handler.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import numpy as np
 
 class Data:
     def __init__(self):
@@ -21,6 +23,30 @@ class Data:
         for c in col_letter:
             num = num * 26 + (ord(c.upper()) - ord('A')) + 1
         return num - 1
+    
+    def plot_ellipse(self, mean, cov, ax, color='k', alpha=0.5):
+        # Exit if the covariance matrix is not positive definite
+        if np.any(np.isnan(cov)):
+            print("Warning: Covariance matrix is not positive definite. Ellipse not plotted.")
+            return
+
+        # Plot an ellipse given a mean and covariance matrix
+        try:
+            eigenvalues, eigenvectors = np.linalg.eig(cov)
+            
+            # Check for negative eigenvalues
+            if np.any(eigenvalues < 0):
+                print("Warning: Negative eigenvalues found. Covariance matrix is not positive definite. Ellipse not plotted.")
+                return
+            
+            angle = np.degrees(np.arctan2(eigenvectors[1, 0], eigenvectors[0, 0]))
+            width, height = 2 * np.sqrt(eigenvalues)
+            ellipse = Ellipse(xy=mean, width=width, height=height, angle=angle, 
+                         color=color, alpha=alpha)
+            ax.add_patch(ellipse)
+        except:
+            print("Warning: Could not plot ellipse.")
+            return
     
 # Wrist data from Longo 2017
 class Data2017(Data):
@@ -55,7 +81,7 @@ class Data2017(Data):
 
         def extract_data(data, rows):
             row_slice = slice(rows[0]-1, rows[1])
-            return {
+            data_dict = {
                 'pre_position': {
                     'X': data.iloc[row_slice, pre_position_cols[0]].astype(float).values,
                     'Y': data.iloc[row_slice, pre_position_cols[1]].astype(float).values,
@@ -88,62 +114,135 @@ class Data2017(Data):
                 },
                 'pix2cm': float(data.iloc[11, 2])
             }
+
+            data_dict['posterior_cov'] = self.covariance_matrix(data_dict)
+            return data_dict
     
         dorsum_data = []
         for i in range(1, 12):
             data = pd.read_csv(f"data/Longo_2017/wrist_data_{i}.csv")
-            dorsum_data.append([extract_data(data, dorsum_1_rows), extract_data(data, dorsum_2_rows)])
+            data1 = extract_data(data, dorsum_1_rows)
+            data2 = extract_data(data, dorsum_2_rows)
+            data3 = self.combine_data(data1, data2)
+            dorsum_data.append([data1, data2, data3])
 
         return dorsum_data
+    
+    def combine_data(self, data1, data2):
+        data3 = {
+            'pre_position': {'X': None, 'Y': None},
+            'post_position': {'X': None, 'Y': None},
+            'mean_position': {'X': None, 'Y': None},
+            'mean_judged': {'X': None, 'Y': None},
+            'indiv_judged': {
+                '1': {'X': None, 'Y': None},
+                '2': {'X': None, 'Y': None},
+                '3': {'X': None, 'Y': None},
+                '4': {'X': None, 'Y': None},
+                '5': {'X': None, 'Y': None},
+                '6': {'X': None, 'Y': None},
+            },
+            'pix2cm': None
+        }
+        # Stack arrays and take mean along first axis
+        data3['pre_position']['X'] = (data1['pre_position']['X'] + data2['pre_position']['X']) / 2
+        data3['pre_position']['Y'] = (data1['pre_position']['Y'] + data2['pre_position']['Y']) / 2
+        data3['post_position']['X'] = (data1['post_position']['X'] + data2['post_position']['X']) / 2
+        data3['post_position']['Y'] = (data1['post_position']['Y'] + data2['post_position']['Y']) / 2
+        data3['mean_position']['X'] = (data1['mean_position']['X'] + data2['mean_position']['X']) / 2
+        data3['mean_position']['Y'] = (data1['mean_position']['Y'] + data2['mean_position']['Y']) / 2
+        data3['mean_judged']['X'] = (data1['mean_judged']['X'] + data2['mean_judged']['X']) / 2
+        data3['mean_judged']['Y'] = (data1['mean_judged']['Y'] + data2['mean_judged']['Y']) / 2
+        data3['indiv_judged']['1']['X'] = data1['indiv_judged']['1']['X']
+        data3['indiv_judged']['1']['Y'] = data1['indiv_judged']['1']['Y']
+        data3['indiv_judged']['2']['X'] = data1['indiv_judged']['2']['X']
+        data3['indiv_judged']['2']['Y'] = data1['indiv_judged']['2']['Y']
+        data3['indiv_judged']['3']['X'] = data1['indiv_judged']['3']['X']
+        data3['indiv_judged']['3']['Y'] = data1['indiv_judged']['3']['Y']
+        data3['indiv_judged']['4']['X'] = data2['indiv_judged']['1']['X']
+        data3['indiv_judged']['4']['Y'] = data2['indiv_judged']['1']['Y']
+        data3['indiv_judged']['5']['X'] = data2['indiv_judged']['2']['X']
+        data3['indiv_judged']['5']['Y'] = data2['indiv_judged']['2']['Y']
+        data3['indiv_judged']['6']['X'] = data2['indiv_judged']['3']['X']
+        data3['indiv_judged']['6']['Y'] = data2['indiv_judged']['3']['Y']
+        data3['pix2cm'] = (data1['pix2cm'] + data2['pix2cm']) / 2
 
+        data3['posterior_cov'] = self.covariance_matrix(data3)
+        return data3  # Add return statement
+    
+    def covariance_matrix(self, data):
+        covs = np.zeros((len(data['mean_judged']['X']), 2, 2))
+        for j in range(len(data['mean_judged']['X'])):
+            x_points = np.array([data['indiv_judged'][str(n+1)]['X'][j]/data['pix2cm'] for n in range(len(data['indiv_judged']))])
+            y_points = np.array([data['indiv_judged'][str(n+1)]['Y'][j]/data['pix2cm'] for n in range(len(data['indiv_judged']))])
+            points = np.vstack([x_points, y_points])
+            cov = np.cov(points)
+            covs[j] = cov
+        return covs
 
     def plot_data(self, participant_num=None):
         # This plot will plot the pre positions of the dorsum, the individual judged positions, and the mean judged position
         # If a participant number is provided, it will only plot the data for that participant
 
-        def plot_connection(ax, data_segment, pix2cm, color):
-            for connection in self.connection_key:
-                ax.plot([data_segment['X'][connection[0]-1]/pix2cm, data_segment['X'][connection[1]-1]/pix2cm],
-                         [data_segment['Y'][connection[0]-1]/pix2cm, data_segment['Y'][connection[1]-1]/pix2cm], color)
-
-        def plot_dorsum_data(dorsum_data, ax, test_num, participant):
-
-            # Plot the pre positions of the dorsum
-            ax.scatter(dorsum_data['pre_position']['X']/dorsum_data['pix2cm'], dorsum_data['pre_position']['Y']/dorsum_data['pix2cm'], label='Pre Position')
-            plot_connection(ax, dorsum_data['pre_position'], dorsum_data['pix2cm'], 'b-')
-
-            # Plot the individual judged positions
-            # colors = ['r', 'g', 'm']
-            # for i in range(1, 4):
-            #     color = colors[i-1]
-            #     ax.scatter(dorsum_data['indiv_judged'][str(i)]['X']/dorsum_data['pix2cm'], dorsum_data['indiv_judged'][str(i)]['Y']/dorsum_data['pix2cm'], label=f'Judged {i}', color=color)
-            #     plot_connection(ax, dorsum_data['indiv_judged'][str(i)], dorsum_data['pix2cm'], color)
-
-            # Plot the mean judged position
-            ax.scatter(dorsum_data['mean_judged']['X']/dorsum_data['pix2cm'], dorsum_data['mean_judged']['Y']/dorsum_data['pix2cm'], label='Mean Judged', color='k')
-            plot_connection(ax, dorsum_data['mean_judged'], dorsum_data['pix2cm'], 'k-')
-            ax.legend()
-            ax.set_aspect('equal', adjustable='box')
-            ax.set_xlabel('X (cm)')
-            ax.set_ylabel('Y (cm)')
-            ax.set_title(f'Participant {participant} Test {test_num}')
-
         if participant_num is not None:
-            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,6))
+            fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12,6))
             dorsum_data = self.dorsum_data[participant_num-1]
-            plot_dorsum_data(dorsum_data[0], ax1, 1, participant_num)
-            plot_dorsum_data(dorsum_data[1], ax2, 2, participant_num)
+            self.plot_dorsum_data(dorsum_data[0], ax1, 1, participant_num)
+            self.plot_dorsum_data(dorsum_data[1], ax2, 2, participant_num)
+            self.plot_dorsum_data(dorsum_data[2], ax3, 3, participant_num)
             fig.show()
 
         else:
             for i in range(len(self.dorsum_data)):
-                fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,6))
+                fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12,6))
                 dorsum_data = self.dorsum_data[i]
-                plot_dorsum_data(dorsum_data[0], ax1, 1, i+1)
-                plot_dorsum_data(dorsum_data[1], ax2, 2, i+1)
+                self.plot_dorsum_data(dorsum_data[0], ax1, 1, i+1)
+                self.plot_dorsum_data(dorsum_data[1], ax2, 2, i+1)
+                self.plot_dorsum_data(dorsum_data[2], ax3, 3, i+1)
                 fig.show()
 
         plt.show()
+
+    def plot_connection(self, ax, data_segment, pix2cm, color, label=None):
+        for i, connection in enumerate(self.connection_key):
+            if i == 0:
+                ax.plot([data_segment['X'][connection[0]-1]/pix2cm, data_segment['X'][connection[1]-1]/pix2cm], [data_segment['Y'][connection[0]-1]/pix2cm, data_segment['Y'][connection[1]-1]/pix2cm], color, label=label)
+            else:
+                ax.plot([data_segment['X'][connection[0]-1]/pix2cm, data_segment['X'][connection[1]-1]/pix2cm], [data_segment['Y'][connection[0]-1]/pix2cm, data_segment['Y'][connection[1]-1]/pix2cm], color)
+
+    def plot_dorsum_data(self, dorsum_data, ax, test_num, participant):
+
+        # Plot the pre positions of the dorsum
+        #ax.scatter(dorsum_data['pre_position']['X']/dorsum_data['pix2cm'], dorsum_data['pre_position']['Y']/dorsum_data['pix2cm'], label='Prior')
+        self.plot_connection(ax, dorsum_data['pre_position'], dorsum_data['pix2cm'], 'b-', label='Prior')
+
+        # Plot the individual judged positions
+        # colors = ['r', 'g', 'm']
+        # for i in range(1, 4):
+        #     color = colors[i-1]
+        #     ax.scatter(dorsum_data['indiv_judged'][str(i)]['X']/dorsum_data['pix2cm'], dorsum_data['indiv_judged'][str(i)]['Y']/dorsum_data['pix2cm'], label=f'Judged {i}', color=color)
+        #     plot_connection(ax, dorsum_data['indiv_judged'][str(i)], dorsum_data['pix2cm'], color)
+        self.plot_wrist_ellipses(dorsum_data['mean_judged']['X']/dorsum_data['pix2cm'], dorsum_data['mean_judged']['Y']/dorsum_data['pix2cm'], dorsum_data['posterior_cov'], ax, 'r', 0.5)
+
+        # Plot the mean judged position
+        #ax.scatter(dorsum_data['mean_judged']['X']/dorsum_data['pix2cm'], dorsum_data['mean_judged']['Y']/dorsum_data['pix2cm'], label='Mean Posterior', color='k')
+        self.plot_connection(ax, dorsum_data['mean_judged'], dorsum_data['pix2cm'], 'k-', label='Posterior')
+        ax.legend()
+        ax.set_aspect('equal', adjustable='box')
+        ax.set_xlabel('X (cm)')
+        ax.set_ylabel('Y (cm)')
+        if test_num == 3:
+            ax.set_title(f'Participant {participant} - Combined')
+        else:
+            ax.set_title(f'Participant {participant} Test {test_num}')
+
+    def plot_wrist_ellipses(self, mean_x_data, mean_y_data, cov_data, ax, color='k', alpha=0.5):
+        # The data needs to be a vertical stack of the x and y positions
+        # There are 3 judgements for each test and two tests per participant
+        # It is assumed that the data is only for one participant
+        means = np.vstack([mean_x_data, mean_y_data])
+        for j in range(len(means[0])):
+            self.plot_ellipse(np.array([means[0,j], means[1,j]]), cov_data[j], ax, color, alpha)
 
 # Palm data from Longo 2016
 class Data2016(Data):
@@ -240,8 +339,6 @@ class Data2016(Data):
 
         pass
 
-
-
 if __name__ == "__main__":
     data = Data2017()
-    data.plot_data()
+    data.plot_data(participant_num=None)

main.py
@@ -1,7 +1,79 @@
-import data_handler
+from data_handler import Data2017
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compute_prior_covariance(Sigma_posterior, Sigma_likelihood, epsilon=1e-6):
+    """
+    Compute prior covariance matrix with numerical stability checks.
+    
+    Args:
+        Sigma_posterior: Posterior covariance matrix
+        Sigma_likelihood: Likelihood covariance matrix
+        epsilon: Small regularization term
+    """
+
+    print(f"Sigma_posterior: {Sigma_posterior}")
+    print(f"Sigma_likelihood: {Sigma_likelihood}")
+    # Ensure matrices are symmetric
+    Sigma_posterior = (Sigma_posterior + Sigma_posterior.T) / 2
+    Sigma_likelihood = (Sigma_likelihood + Sigma_likelihood.T) / 2
+    
+    # Add small regularization to ensure matrices are positive definite
+    reg_term = epsilon * np.eye(Sigma_posterior.shape[0])
+    Sigma_posterior += reg_term
+    Sigma_likelihood += reg_term
+    
+    # Compute inverse matrices
+    Sigma_posterior_inv = np.linalg.inv(Sigma_posterior)
+    Sigma_likelihood_inv = np.linalg.inv(Sigma_likelihood)
+    
+    # Compute prior inverse
+    Sigma_prior_inv = Sigma_posterior_inv - Sigma_likelihood_inv
+    
+    # Add regularization if needed
+    if not np.all(np.linalg.eigvals(Sigma_prior_inv) > 0):
+        Sigma_prior_inv += reg_term
+        
+    # Compute prior covariance
+    Sigma_prior = np.linalg.inv(Sigma_prior_inv)
+    
+    return Sigma_prior
+
 
 if __name__ == "__main__":
-    data = data_handler.import_2016()
-    
-    # Make a plot of the data
-    data_handler.plot_data(data)
+
+    #### Hyperparameters ####
+    # The likelihood covariance matrix
+    Sigma_likelihood = np.array([[10.0, 0.0], [0.0, 10.0]])
+    participants = [7,9]
+
+    #########################
+
+    data = Data2017() # Load in the dorsum data
+
+    # We are focused on the combined data from test 1 and test 2
+    for participant in participants:
+        combined_data = data.dorsum_data[participant-1][2]
+        combined_data['prior_cov'] = []
+
+        # Get the prior covariance matrix using the following formula:
+        # Sigma_prior = (Sigma_posterior - Sigma_likelihood)^-1
+        for j in range(8):
+            print(f"\nIndex {j}")
+            Sigma_prior = compute_prior_covariance(combined_data['posterior_cov'][j], Sigma_likelihood)
+            combined_data['prior_cov'].append(Sigma_prior)
+            print(f"Sigma_prior: {Sigma_prior}")
+
+            mew_posterior = np.array([combined_data['mean_judged']['X'][j]/combined_data['pix2cm'], combined_data['mean_judged']['Y'][j]/combined_data['pix2cm']])
+            print(f"mew_posterior: {mew_posterior}")
+            mew_likelihood = np.array([combined_data['pre_position']['X'][j]/combined_data['pix2cm'], combined_data['pre_position']['Y'][j]/combined_data['pix2cm']])
+            print(f"mew_likelihood: {mew_likelihood}")
+            mew_prior = Sigma_prior @ (np.linalg.inv(combined_data['posterior_cov'][j]) @ mew_posterior - np.linalg.inv(Sigma_likelihood) @ mew_likelihood)
+            print(f"mew_prior: {mew_prior}")
+
+        fig, ax = plt.subplots(figsize=(8, 6))
+        data.plot_dorsum_data(combined_data, ax, 3, participant)
+        data.plot_wrist_ellipses(combined_data['pre_position']['X']/combined_data['pix2cm'], combined_data['pre_position']['Y']/combined_data['pix2cm'], combined_data['prior_cov'], ax, 'b', 0.5)
+        plt.show()
+
+